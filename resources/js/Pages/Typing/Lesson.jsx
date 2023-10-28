4ae4158bd3adf6a8916be22f02348e7163f01b09
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-10-28 21:35:36 +0300

    now the keyboard and the hands are shown based on the users settings

--- resources/js/Pages/Typing/Lesson.jsx
+++ resources/js/Pages/Typing/Lesson.jsx
@@ -202,20 +202,64 @@ export default function Lesson({ screen, locale, exerciseTotalStars }) {
                 </p>
 
                 {/* start images */}
-                <div className="flex flex-row justify-center relative w-full">
-                    {screen.content.split("").map((char, i) => {
-                        let fingerClass = "absolute -right-28 -top-7 w-4/5"; // Initialize hand class
+                {user_settings.show_hands === true ? (
+                    <div className="flex flex-row justify-center relative w-full">
+                        {screen.content.split("").map((char, i) => {
+                            let fingerClass = "absolute -right-28 -top-7 w-4/5"; // Initialize hand class
 
-                        if (macLeftKeys.includes(char.toLowerCase())) {
-                            fingerClass = "absolute -left-44 -top-14 w-4/5"; // Assign left-hand class
-                        } else if (macRightKeys.includes(char.toLowerCase())) {
-                            fingerClass = "absolute -right-28 -top-7 w-4/5"; // Assign right-hand class
-                        }
+                            if (macLeftKeys.includes(char.toLowerCase())) {
+                                fingerClass = "absolute -left-44 -top-14 w-4/5"; // Assign left-hand class
+                            } else if (
+                                macRightKeys.includes(char.toLowerCase())
+                            ) {
+                                fingerClass = "absolute -right-28 -top-7 w-4/5"; // Assign right-hand class
+                            }
 
-                        if (char === " ") {
+                            if (char === " ") {
+                                const fingerImage =
+                                    macFingerMapping[char] || ""; // Get the finger image
+                                return (
+                                    <span key={i}>
+                                        {char === currentCharacter && (
+                                            <img
+                                                src={fingerImage}
+                                                alt=""
+                                                className={fingerClass} // Apply your styling for the finger image here
+                                            />
+                                        )}
+                                    </span>
+                                );
+                            }
+
                             const fingerImage = macFingerMapping[char] || ""; // Get the finger image
+                            let shiftImage = null;
+                            let shiftFingerClass =
+                                "absolute -right-28 -top-7 w-4/5"; // Initialize hand class
+
+                            // Handle characters that require the Shift key
+                            if (macRightShiftKeys.includes(char)) {
+                                shiftImage = "/img/fingers/right-shift.webp"; // Set the image for the right Shift key
+                                shiftFingerClass =
+                                    "absolute -right-28 -top-7 w-4/5";
+                            } else if (macLeftShiftKeys.includes(char)) {
+                                shiftImage = "/img/fingers/left-shift.webp"; // Set the image for the left Shift key
+                                shiftFingerClass =
+                                    "absolute -left-44 -top-14 w-4/5";
+                            }
+
+                            // Determine if the shiftImage should be shown
+                            const shouldDisplayShiftImage =
+                                shiftImage && char === currentCharacter;
+
                             return (
                                 <span key={i}>
+                                    {shouldDisplayShiftImage && (
+                                        <img
+                                            src={shiftImage}
+                                            alt=""
+                                            className={shiftFingerClass} // Apply your styling for the finger image here
+                                        />
+                                    )}
                                     {char === currentCharacter && (
                                         <img
                                             src={fingerImage}
@@ -225,58 +269,27 @@ export default function Lesson({ screen, locale, exerciseTotalStars }) {
                                     )}
                                 </span>
                             );
-                        }
-
-                        const fingerImage = macFingerMapping[char] || ""; // Get the finger image
-                        let shiftImage = null;
-                        let shiftFingerClass =
-                            "absolute -right-28 -top-7 w-4/5"; // Initialize hand class
-
-                        // Handle characters that require the Shift key
-                        if (macRightShiftKeys.includes(char)) {
-                            shiftImage = "/img/fingers/right-shift.webp"; // Set the image for the right Shift key
-                            shiftFingerClass =
-                                "absolute -right-28 -top-7 w-4/5";
-                        } else if (macLeftShiftKeys.includes(char)) {
-                            shiftImage = "/img/fingers/left-shift.webp"; // Set the image for the left Shift key
-                            shiftFingerClass =
-                                "absolute -left-44 -top-14 w-4/5";
-                        }
-
-                        // Determine if the shiftImage should be shown
-                        const shouldDisplayShiftImage =
-                            shiftImage && char === currentCharacter;
-
-                        return (
-                            <span key={i}>
-                                {shouldDisplayShiftImage && (
-                                    <img
-                                        src={shiftImage}
-                                        alt=""
-                                        className={shiftFingerClass} // Apply your styling for the finger image here
-                                    />
-                                )}
-                                {char === currentCharacter && (
-                                    <img
-                                        src={fingerImage}
-                                        alt=""
-                                        className={fingerClass} // Apply your styling for the finger image here
-                                    />
-                                )}
-                            </span>
-                        );
-                    })}
-                </div>
-                {locale === "ckb" ? (
-                    user_settings.keyboard_type === "windows" ? (
-                        <WindowsKeyboardKu
-                            currentCharacter={currentCharacter}
-                        />
+                        })}
+                    </div>
+                ) : (
+                    <></>
+                )}
+                {user_settings.show_keyboard === true ? (
+                    locale === "ckb" ? (
+                        user_settings.keyboard_type === "windows" ? (
+                            <WindowsKeyboardKu
+                                currentCharacter={currentCharacter}
+                            />
+                        ) : (
+                            <MacKeyboardKu
+                                currentCharacter={currentCharacter}
+                            />
+                        )
                     ) : (
-                        <MacKeyboardKu currentCharacter={currentCharacter} />
+                        <MacKeyboardEn currentCharacter={currentCharacter} />
                     )
                 ) : (
-                    <MacKeyboardEn currentCharacter={currentCharacter} />
+                    <></>
                 )}
 
                 <Modal show={modalOpen} onClose={closeModal}>
