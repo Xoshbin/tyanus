d1a0d84cf202af85531d35ff0efd57263e0daff7
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-10-29 22:08:22 +0300

    optimized the progress header to not query the database if user is not logged in

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\UserSettings;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 use App\Models\UserProgress;
@@ -23,25 +24,32 @@ class LessonsController extends Controller
         });
 
         $daily_time = $userSettings->getDailyTime();
-        $userProgress = UserProgress::where('user_id', auth()->id())->select('time', 'typing_speed', 'accuracy_percentage')->get();
-        $userProgressToday = UserProgress::where('user_id', auth()->id())->whereDate('created_at', Carbon::today())->select('time')->get();
+        if (Auth::check()) {
+            $userProgress = UserProgress::where('user_id', auth()->id())->select('time', 'typing_speed', 'accuracy_percentage')->get();
+            $userProgressToday = UserProgress::where('user_id', auth()->id())->whereDate('created_at', Carbon::today())->select('time')->get();
 
-        $todayTime = $userProgressToday->sum('time');
+            $todayTime = $userProgressToday->sum('time');
 
-        // Create a Carbon instance from the counter time in seconds
-        $carbonTime = Carbon::createFromTimestamp($todayTime);
-        // Check if the duration is less than an hour
-        if ($carbonTime->hour < 1) {
-            // Format the Carbon instance to display in "minutes:seconds" format
-            $todaySumTime = $carbonTime->format('i:s');
+            // Create a Carbon instance from the counter time in seconds
+            $carbonTime = Carbon::createFromTimestamp($todayTime);
+            // Check if the duration is less than an hour
+            if ($carbonTime->hour < 1) {
+                // Format the Carbon instance to display in "minutes:seconds" format
+                $todaySumTime = $carbonTime->format('i:s');
+            } else {
+                // Format the Carbon instance to display in "hours:minutes:seconds" format
+                $todaySumTime = $carbonTime->format('H:i:s');
+            }
+
+            $sumTime = $userProgress->sum('time');
+            $avgSpeed = $userProgress->avg('typing_speed');
+            $avgAccuracy = $userProgress->avg('accuracy_percentage');
         } else {
-            // Format the Carbon instance to display in "hours:minutes:seconds" format
-            $todaySumTime = $carbonTime->format('H:i:s');
+            $todaySumTime = 0;
+            $sumTime = 0;
+            $avgSpeed = 0;
+            $avgAccuracy = 0;
         }
-
-        $sumTime = $userProgress->sum('time');
-        $avgSpeed = $userProgress->avg('typing_speed');
-        $avgAccuracy = $userProgress->avg('accuracy_percentage');
 
         return Inertia::render('Typing/Lessons', [
             'lessons' => $lessons,
