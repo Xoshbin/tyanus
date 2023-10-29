cf4038ab841afda13951d7e8e3734a2553d80a79
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-10-30 00:24:28 +0300

    add user progress service to save user progress and retrieve it

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Exercise;
 use App\Models\Screen;
-use Illuminate\Support\Facades\Auth;
+use App\Services\UserProgressService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\App;
-use App\Models\UserProgress;
-use Illuminate\Support\Str;
 
 class LessonController extends Controller
 {
@@ -44,44 +41,8 @@ class LessonController extends Controller
 
     public function saveProgress(Request $request)
     {
-        if (Auth::check()) {
-            $data = $request->validate([
-                'lesson_id' => 'required',
-                'exercise_id' => 'required',
-                'screen_id' => 'required',
-                'locale' => 'required',
-                'typing_speed' => 'required',
-                'accuracy_percentage' => 'required',
-                'stars_earned' => 'required',
-                'time' => 'required',
-            ]);
+        $userProgressService = new UserProgressService;
 
-            $data['user_id'] = auth()->user()->id;
-
-            UserProgress::create($data);
-        } else {
-            // Check if a guest user ID already exists in the session
-            $guestUserId = session('guest_user_id');
-
-            if (!$guestUserId) {
-                // Generate a unique identifier for the guest user
-                $guestUserId = Str::uuid()->toString();
-
-                // Store the guest user ID in the session
-                session(['guest_user_id' => $guestUserId]);
-            }
-
-            // Retrieve the existing progress data for the guest user (if any)
-            $progressData = session('progress_' . $guestUserId, []);
-
-            // Extract the screen ID from the request
-            $screenId = $request->input('screen_id');
-
-            // Add the current progress data to the specific screen's array
-            $progressData[$screenId][] = $request->all();
-
-            // Store the updated progress data back in the session
-            session(['progress_' . $guestUserId => $progressData]);
-        }
+        $userProgressService->setProgress($request);
     }
 }
--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cache;
 use App\Models\Lesson;
+use App\Services\UserProgressService;
 
 class LessonsController extends Controller
 {
@@ -18,38 +19,18 @@ class LessonsController extends Controller
     function index(): Response
     {
         $userSettings = new UserSettings;
+        $userProgressService = new UserProgressService;
         $exerciseLanguage = $userSettings->getExerciseLang();
         $lessons = Cache::rememberForever('lessons' . $exerciseLanguage, function () use ($exerciseLanguage) {
             return Lesson::where('locale', $exerciseLanguage)->where('active', true)->with('exercises.screens.userProgress')->get();
         });
 
         $daily_time = $userSettings->getDailyTime();
-        if (Auth::check()) {
-            $userProgress = UserProgress::where('user_id', auth()->id())->select('time', 'typing_speed', 'accuracy_percentage')->get();
-            $userProgressToday = UserProgress::where('user_id', auth()->id())->whereDate('created_at', Carbon::today())->select('time')->get();
 
-            $todayTime = $userProgressToday->sum('time');
-
-            // Create a Carbon instance from the counter time in seconds
-            $carbonTime = Carbon::createFromTimestamp($todayTime);
-            // Check if the duration is less than an hour
-            if ($carbonTime->hour < 1) {
-                // Format the Carbon instance to display in "minutes:seconds" format
-                $todaySumTime = $carbonTime->format('i:s');
-            } else {
-                // Format the Carbon instance to display in "hours:minutes:seconds" format
-                $todaySumTime = $carbonTime->format('H:i:s');
-            }
-
-            $sumTime = $userProgress->sum('time');
-            $avgSpeed = $userProgress->avg('typing_speed');
-            $avgAccuracy = $userProgress->avg('accuracy_percentage');
-        } else {
-            $todaySumTime = 0;
-            $sumTime = 0;
-            $avgSpeed = 0;
-            $avgAccuracy = 0;
-        }
+        $todaySumTime = $userProgressService->getTodaySumTime();
+        $sumTime = $userProgressService->getSumTime();
+        $avgSpeed = $userProgressService->getAvgSpeed();
+        $avgAccuracy = $userProgressService->getAvgAccuracy();
 
         return Inertia::render('Typing/Lessons', [
             'lessons' => $lessons,
