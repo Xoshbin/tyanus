0592df41bb9e4c5ca7988223b44700c726f8baf8
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-11-03 11:01:27 +0300

    changed the design of exercie cards

--- resources/js/Components/MaterialD/ExerciseCard.jsx
+++ resources/js/Components/MaterialD/ExerciseCard.jsx
@@ -7,18 +7,22 @@ import {
 } from "@material-tailwind/react";
 import { usePage } from "@inertiajs/react";
 import moment from "moment";
+import humanizeDuration from "humanize-duration";
 import { __ } from "@/Libs/Lang";
 import StarIcon from "@/Components/Typing/StarIcon";
 
 export function ExerciseCard({ screen }) {
     const { locale } = usePage().props;
+    const duration = moment.duration(screen.time, "seconds");
 
     return (
         <Card
             color="transparent"
             shadow={false}
             className={`w-60 p-4 ${
-                screen.time >= 1 ? "bg-kblue-400" : "bg-kblue-200"
+                screen.time <= 1
+                    ? "bg-gradient-to-l from-kblue-500 to-kblue-600"
+                    : "bg-gradient-to-l from-kblue-300 to-kblue-400"
             }`}
         >
             <CardHeader
@@ -29,7 +33,12 @@ export function ExerciseCard({ screen }) {
             >
                 <div className="flex w-full flex-col gap-0.5">
                     <div className="flex flex-col items-center justify-between">
-                        <Typography variant="h5" color="blue-gray">
+                        <Typography
+                            variant="h5"
+                            color={`${
+                                screen.time <= 1 ? "white" : "neutral-600"
+                            }`}
+                        >
                             {screen.title}
                         </Typography>
                         <div className="5 flex items-center gap-0">
@@ -44,20 +53,26 @@ export function ExerciseCard({ screen }) {
                 </div>
             </CardHeader>
             <CardBody className="flex mb-0 p-0 justify-between items-end">
-                <div className={`flex flex-col`}>
+                <div
+                    className={`flex flex-col ${
+                        screen.time <= 1 ? "text-white" : "text-neutral-600"
+                    } `}
+                >
                     <Typography variant="h6">
-                        {moment
-                            .duration(screen.time, "milliseconds")
-                            .locale("ku")
-                            .humanize()}
+                        {humanizeDuration(duration.asMilliseconds(), {
+                            units: ["d", "h", "m", "s"],
+                            largest: 2,
+                            language: "ku",
+                        })}
                     </Typography>
                     <Typography variant="h6">
                         {locale === "ckb" ? "دروستی" : "Accuracy"}: %{" "}
-                        {screen.accuracyPercentage}
+                        {parseInt(screen.accuracyPercentage)}
                     </Typography>
                     <Typography variant="h6">
                         {locale === "ckb" ? "خێرایی" : "Speed"}:{" "}
-                        {screen.typingSpeed} {locale === "ckb" ? "ولخ" : "WPM"}{" "}
+                        {parseInt(screen.typingSpeed)}{" "}
+                        {locale === "ckb" ? "ولخ" : "WPM"}{" "}
                     </Typography>
                 </div>
             </CardBody>
