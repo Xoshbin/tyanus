abd5d192c22f0d47d1947490af96bd3a996e034d
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-11-03 11:36:01 +0300

    add language picker in the navbar

--- resources/js/Components/Typing/Navbar.jsx
+++ resources/js/Components/Typing/Navbar.jsx
@@ -6,7 +6,7 @@ import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { __ } from "@/Libs/Lang";
 
-export default function Navbar(locale) {
+export default function Navbar({ locale }) {
     const { auth } = usePage().props;
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
@@ -38,6 +38,44 @@ export default function Navbar(locale) {
                             >
                                 {__("Test")}
                             </NavLink>
+                            <Dropdown>
+                                <Dropdown.Trigger>
+                                    <span className="inline-flex rounded-md">
+                                        <button
+                                            type="button"
+                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                        >
+                                            {locale}
+
+                                            <svg
+                                                className="ml-2 -mr-0.5 h-4 w-4"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </Dropdown.Trigger>
+
+                                <Dropdown.Content>
+                                    <Dropdown.Link
+                                        href={route("setlocale", "ckb")}
+                                    >
+                                        {__("KU")}
+                                    </Dropdown.Link>
+                                    <Dropdown.Link
+                                        href={route("setlocale", "en")}
+                                    >
+                                        {__("EN")}
+                                    </Dropdown.Link>
+                                </Dropdown.Content>
+                            </Dropdown>
                         </div>
                         <div className="ml-3 relative">
                             {auth.user ? (
