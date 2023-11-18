e538e21359eb86d62d83b2f1bb251ce75f94bbcc
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-11-18 22:57:45 +0300

    changed the navbar to be more compatible with Kurdish layout

--- resources/js/Components/Typing/Navbar.jsx
+++ resources/js/Components/Typing/Navbar.jsx
@@ -12,71 +12,10 @@ export default function Navbar({ locale }) {
         useState(false);
     return (
         <nav>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" dir="ltr">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8" dir="ltr">
                 <div className="flex justify-between h-16">
-                    <div className="flex">
-                        <div className="shrink-0 flex items-center">
-                            <Link href="/">
-                                <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                            </Link>
-                        </div>
-                    </div>
-
+                    {/* Auth */}
                     <div className="hidden sm:flex sm:items-center sm:ml-6">
-                        <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                            <NavLink
-                                className="flex text-xl border-2 border-transparent rounded-full transition text-kblue-600"
-                                href={route("lessons")}
-                                active={route().current("lessons")}
-                            >
-                                {__("Lessons")}
-                            </NavLink>
-                            <NavLink
-                                className="flex text-xl border-2 border-transparent rounded-full transition text-kblue-600"
-                                href={route("test")}
-                                active={route().current("test")}
-                            >
-                                {__("Test")}
-                            </NavLink>
-                            {/* <Dropdown>
-                                <Dropdown.Trigger>
-                                    <span className="inline-flex rounded-md">
-                                        <button
-                                            type="button"
-                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                        >
-                                            {locale}
-
-                                            <svg
-                                                className="ml-2 -mr-0.5 h-4 w-4"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </span>
-                                </Dropdown.Trigger>
-
-                                <Dropdown.Content>
-                                    <Dropdown.Link
-                                        href={route("setlocale", "ckb")}
-                                    >
-                                        {__("KU")}
-                                    </Dropdown.Link>
-                                    <Dropdown.Link
-                                        href={route("setlocale", "en")}
-                                    >
-                                        {__("EN")}
-                                    </Dropdown.Link>
-                                </Dropdown.Content>
-                            </Dropdown> */}
-                        </div>
                         <div className="ml-3 relative">
                             {auth.user ? (
                                 <Dropdown>
@@ -144,6 +83,72 @@ export default function Navbar({ locale }) {
                                     </Link>
                                 </>
                             )}
+                        </div>
+                    </div>
+
+                    <div className="flex sm:items-center sm:ml-6 sm:space-x-10">
+                        {/* Menus */}
+                        <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <NavLink
+                                className="flex text-xl border-2 border-transparent rounded-full transition text-kblue-600"
+                                href={route("lessons")}
+                                active={route().current("lessons")}
+                            >
+                                {__("Lessons")}
+                            </NavLink>
+                            <NavLink
+                                className="flex text-xl border-2 border-transparent rounded-full transition text-kblue-600"
+                                href={route("test")}
+                                active={route().current("test")}
+                            >
+                                {__("Test")}
+                            </NavLink>
+                            {/* <Dropdown>
+                                <Dropdown.Trigger>
+                                    <span className="inline-flex rounded-md">
+                                        <button
+                                            type="button"
+                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                        >
+                                            {locale}
+
+                                            <svg
+                                                className="ml-2 -mr-0.5 h-4 w-4"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </Dropdown.Trigger>
+
+                                <Dropdown.Content>
+                                    <Dropdown.Link
+                                        href={route("setlocale", "ckb")}
+                                    >
+                                        {__("KU")}
+                                    </Dropdown.Link>
+                                    <Dropdown.Link
+                                        href={route("setlocale", "en")}
+                                    >
+                                        {__("EN")}
+                                    </Dropdown.Link>
+                                </Dropdown.Content>
+                            </Dropdown> */}
+                        </div>
+                        {/* Logo */}
+                        <div className="flex">
+                            <div className="shrink-0 flex items-center">
+                                <Link href="/">
+                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                </Link>
+                            </div>
                         </div>
                     </div>
 
