49179e5e9101827206fd08daf8635c49722a09ca
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-12-03 21:32:20 +0300

    add google tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,18 +2,24 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @if (app()->getLocale() == 'ckb') dir="rtl" @endif>
 
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0Z9K10WHXQ"></script>
+    <!-- Google Tag Manager -->
     <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-0Z9K10WHXQ');
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KLF4M88Z');
     </script>
+    <!-- End Google Tag Manager -->
 
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6978489323266231"
         crossorigin="anonymous"></script>
@@ -48,6 +54,10 @@
 </head>
 
 <body class="font-sans antialiased">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLF4M88Z" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @inertia
 </body>
 
