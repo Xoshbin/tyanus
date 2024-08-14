b56e478f0d69a84fcde2d9c91e0d735224c3be2b
Khoshbin <xoshbin@gmail.com>
2024-08-14 13:05:50 +0300

    Explain in the hero that the typing are both in Kurdish and English

--- resources/js/Components/Typing/HomePage/Hero.jsx
+++ resources/js/Components/Typing/HomePage/Hero.jsx
@@ -22,6 +22,11 @@ export default function Hero({ locale }) {
                             alt=""
                         />
                     </div>
+                    <h2 className="text-3xl text-kyellow-500 font-semibold antialiased writer-text underline">
+                        {__(
+                            "In Kurdish and English"
+                        )}
+                    </h2>
                     <h2 className="text-3xl text-kblue-600 font-semibold antialiased writer-text">
                         {__("Type Faster, Work Smarter!")}
                     </h2>
@@ -44,7 +49,8 @@ export default function Hero({ locale }) {
                     href={route("lessons")}
                     className="relative inline-block text-lg group"
                 >
-                    <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                    <span
+                        className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                         <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-kyellow-50"></span>
                         <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-kyellow-400 group-hover:-rotate-180 ease"></span>
                         <span className="relative flex items-center @if (app()->getLocale() == 'ckb') space-x-reverse @endif space-x-4">
