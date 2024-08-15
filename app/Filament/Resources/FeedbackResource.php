5b1996083c0f311872e7e22cc62db6719c9a547e
Khoshbin <xoshbin@gmail.com>
2024-08-15 12:53:07 +0300

    Add feedback filament resource

---
+++ app/Filament/Resources/FeedbackResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FeedbackResource\Pages;
+use App\Filament\Resources\FeedbackResource\RelationManagers;
+use App\Models\Feedback;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FeedbackResource extends Resource
+{
+    protected static ?string $model = Feedback::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('message')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('message')->wrap()->limit(25)->searchable() // Allows the values in this column to be searched.
+                ->label('فیدباک'),
+                Tables\Columns\CheckboxColumn::make('is_read')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFeedback::route('/'),
+            'create' => Pages\CreateFeedback::route('/create'),
+            'edit' => Pages\EditFeedback::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return Feedback::where('is_read', false)->count();
+    }
+
+    public static function getNavigationBadgeColor(): ?string
+    {
+        return 'warning';
+    }
+}
